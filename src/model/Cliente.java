7574ac15a6e9f1c6d56b0fe8475c9a35d0cd27db
NICOLASVASQUEZ17 <nicolasvasquezorrego5@gmail.com>
2023-03-10 16:32:30 -0500

    falta el main

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,2 +1,72 @@
-package model;public class Persona {
+package model;
+
+public class Cliente {
+
+    //Atributos
+
+    private String nombre;
+    private String ocupacion;
+    private String dni;
+    private String genero;
+    private CuentaBancaria cuenteaBancaria;
+
+    public Cliente(String nombre, String ocupacion, String dni, String genero) {
+        this.nombre = nombre;
+        this.ocupacion = ocupacion;
+        this.dni = dni;
+        this.genero = genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getOcupacion() {
+        return ocupacion;
+    }
+
+    public void setOcupacion(String ocupacion) {
+        this.ocupacion = ocupacion;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public CuentaBancaria getCuenteaBancaria() {
+        return cuenteaBancaria;
+    }
+
+    public void setCuenteaBancaria(CuentaBancaria cuenteaBancaria) {
+        this.cuenteaBancaria = cuenteaBancaria;
+    }
+
+    public void consultarSaldo(Transaccion transaccion){
+
+
+        System.out.printf("su saldo es: "+ transaccion.getSaldo()}t6);
+    }
+
+    public void procesarTransaccion(Transaccion transaccion){
+
+        transaccion.ejcutarTransaccion();
+
+    }
+
 }
